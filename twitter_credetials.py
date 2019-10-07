963feef58d6904a555ef647731f7edb911024a08
afihidyat <rafli.hidayat99@gmail.com>
2019-10-07 19:56:40 +0700

    Menambahkan fitur live streaming twitter

---
+++ twitter_credetials.py
@@ -0,0 +1,5 @@
+# Variables that contain the user credentials to access twitter API
+ACCESS_TOKEN = "103190390-O3YasEybxMuQs4eGuVo7Cty0GYkCQuVmrU8u8deq"
+ACCESS_TOKEN_SECRET = "FbbiRSgBDky3A4jHyuid2Fbxo72AGMwiiy76p37IvNmka"
+CONSUMER_KEY = "IlZ0a3CpHmi3kUxFJcyla0wI1"
+CONSUMER_SECRET = "FLLLQDYPAoox8scyDh5a4bpe97H6OGnlhXY8WnQNaJVuuNwupC"
