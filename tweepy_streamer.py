b9389ba1fe1d8034a246b6b44a94363f4256ce28
afihidyat <rafli.hidayat99@gmail.com>
2019-10-09 18:55:17 +0700

    Adding number of analysis

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -1,6 +1,11 @@
+from tweepy import API 
+from tweepy import Cursor
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+ 
+import numpy as np
+import pandas as pd
  
 # import twitter_credentials
 ACCESS_TOKEN = "103190390-O3YasEybxMuQs4eGuVo7Cty0GYkCQuVmrU8u8deq"
@@ -9,19 +14,56 @@ CONSUMER_KEY = "IlZ0a3CpHmi3kUxFJcyla0wI1"
 CONSUMER_SECRET = "FLLLQDYPAoox8scyDh5a4bpe97H6OGnlhXY8WnQNaJVuuNwupC"
 
  
+# # # # TWITTER CLIENT # # # #
+class TwitterClient():
+    def __init__(self, twitter_user=None):
+        self.auth = TwitterAuthenticator().authenticate_twitter_app()
+        self.twitter_client = API(self.auth)
+
+        self.twitter_user = twitter_user
+
+    def get_twitter_client_api(self):
+        return self.twitter_client
+
+    def get_user_timeline_tweets(self, num_tweets):
+        tweets = []
+        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
+            tweets.append(tweet)
+        return tweets
+
+    def get_friend_list(self, num_friends):
+        friend_list = []
+        for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
+            friend_list.append(friend)
+        return friend_list
+
+    def get_home_timeline_tweets(self, num_tweets):
+        home_timeline_tweets = []
+        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
+            home_timeline_tweets.append(tweet)
+        return home_timeline_tweets
+
+
+# # # # TWITTER AUTHENTICATER # # # #
+class TwitterAuthenticator():
+
+    def authenticate_twitter_app(self):
+        auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+        return auth
+
 # # # # TWITTER STREAMER # # # #
 class TwitterStreamer():
     """
     Class for streaming and processing live tweets.
     """
     def __init__(self):
-        pass
+        self.twitter_autenticator = TwitterAuthenticator()    
 
     def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
         # This handles Twitter authetification and the connection to Twitter Streaming API
-        listener = StdOutListener(fetched_tweets_filename)
-        auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+        listener = TwitterListener(fetched_tweets_filename)
+        auth = self.twitter_autenticator.authenticate_twitter_app() 
         stream = Stream(auth, listener)
 
         # This line filter Twitter Streams to capture data by the keywords: 
@@ -29,7 +71,7 @@ class TwitterStreamer():
 
 
 # # # # TWITTER STREAM LISTENER # # # #
-class StdOutListener(StreamListener):
+class TwitterListener(StreamListener):
     """
     This is a basic listener that just prints received tweets to stdout.
     """
@@ -46,16 +88,54 @@ class StdOutListener(StreamListener):
             print("Error on_data %s" % str(e))
         return True
           
-
     def on_error(self, status):
+        if status == 420:
+            # Returning False on_data method in case rate limit occurs.
+            return False
         print(status)
+
+
+class TweetAnalyzer():
+    """
+    Functionality for analyzing and categorizing content from tweets.
+    """
+    def tweets_to_data_frame(self, tweets):
+        df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+
+        df['id'] = np.array([tweet.id for tweet in tweets])
+        df['len'] = np.array([len(tweet.text) for tweet in tweets])
+        df['date'] = np.array([tweet.created_at for tweet in tweets])
+        df['source'] = np.array([tweet.source for tweet in tweets])
+        df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+
+        return df
 
  
 if __name__ == '__main__':
- 
-    # Authenticate using config.py and connect to Twitter Streaming API.
-    hash_tag_list = ["gempa"]
-    fetched_tweets_filename = "tweets.txt"
 
-    twitter_streamer = TwitterStreamer()
-    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
+    twitter_client = TwitterClient()
+    tweet_analyzer = TweetAnalyzer()
+
+    api = twitter_client.get_twitter_client_api()
+
+    tweets = api.user_timeline(screen_name="Banjir", count=20)
+
+    #print(dir(tweets[0]))
+    #print(tweets[0].retweet_count)
+
+    df = tweet_analyzer.tweets_to_data_frame(tweets)
+    
+    print(df.head(10))
+    print("Average Length:")
+     # Get average length over all tweets:
+    print( np.mean(df['len']))
+
+    print("Most like:")
+
+    # Get the number of likes for the most liked tweet:
+    print(np.max(df['likes']))
+    print("Most tweet:")
+
+    # Get the number of retweets for the most retweeted tweet:
+    print(np.max(df['retweets']))
